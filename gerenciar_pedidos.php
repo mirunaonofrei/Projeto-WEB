acedba22b5a731a55810560e4352fc51209f9cfd
mirunaonofrei <miruna@edu.univali.br>
2025-03-20 17:19:22 -0300

    Removendo edição em linha e implementando toolbar

--- gerenciar_pedidos.php
+++ gerenciar_pedidos.php
@@ -74,12 +74,11 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
     <div class="container">
         <table id="dg" class="easyui-datagrid" style="width:800px;height:400px;"
             data-options="
+            iconCls: 'icon-edit',
             singleSelect:true, 
             fitColumns:true, 
             view:detailview,
-            toolbar: '#tb',
-            onClickCell: onClickCell,
-            onEndEdit: onEndEdit">
+            toolbar: '#tb',">
             <thead>
                 <tr>
                     <th data-options="field:'num_pedido', width:80">Pedido</th>
@@ -91,9 +90,8 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
     <div id="tb" style="height:auto">
         <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-add',plain:true" onclick="append()">Adicionar</a>
         <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-remove',plain:true" onclick="removeit()">Remover</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-save',plain:true" onclick="acceptit()">Salvar</a>
+        <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-edit',plain:true" onclick="edit()">Editar</a>
         <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-undo',plain:true" onclick="reject()">Cancelar</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-search',plain:true" onclick="getChanges()">Ver Mudanças</a>
     </div>
 
 
@@ -113,19 +111,16 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
             onExpandRow: function(index, row) {
                 var ddv = $(this).datagrid('getRowDetail', index).find('div.ddv');
                 ddv.panel({
-                    fitColumns: true,
-                    singleSelect: true,
-                    rownumbers: true,
-                    loadMsg: '',
-                    height: 'auto',
+                    href: 'get_itens_pedido.php?num_pedido=' + row.num_pedido,
                     border: false,
                     cache: false,
-                    href: 'get_itens_pedido.php?num_pedido=' + row.num_pedido,
                     onLoad: function() {
                         $('#dg').datagrid('fixDetailRowHeight', index);
                     }
                 });
-                $('#dg').datagrid('fixDetailRowHeight', index);
+                setTimeout(function() {
+                    $('#dg').datagrid('fixDetailRowHeight', index);
+                }, 0);
             }
         });
 
@@ -172,57 +167,57 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
 
     var editIndex = undefined;
 
-    function endEditing() {
-        if (editIndex == undefined) {
-            return true
-        }
-        if ($('#dg').datagrid('validateRow', editIndex)) {
-            $('#dg').datagrid('endEdit', editIndex);
-            editIndex = undefined;
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // function endEditing() {
+    //     if (editIndex == undefined) {
+    //         return true
+    //     }
+    //     if ($('#dg').datagrid('validateRow', editIndex)) {
+    //         $('#dg').datagrid('endEdit', editIndex);
+    //         editIndex = undefined;
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
-    function onClickCell(index, field) {
-        if (editIndex != index) {
-            if (endEditing()) {
-                $('#dg').datagrid('selectRow', index)
-                    .datagrid('beginEdit', index);
-                var ed = $('#dg').datagrid('getEditor', {
-                    index: index,
-                    field: field
-                });
-                if (ed) {
-                    ($(ed.target).data('textbox') ? $(ed.target).textbox('textbox') : $(ed.target)).focus();
-                }
-                editIndex = index;
-            } else {
-                setTimeout(function() {
-                    $('#dg').datagrid('selectRow', editIndex);
-                }, 0);
-            }
-        }
-    }
+    // function onClickCell(index, field) {
+    //     if (editIndex != index) {
+    //         if (endEditing()) {
+    //             $('#dg').datagrid('selectRow', index)
+    //                 .datagrid('beginEdit', index);
+    //             var ed = $('#dg').datagrid('getEditor', {
+    //                 index: index,
+    //                 field: field
+    //             });
+    //             if (ed) {
+    //                 ($(ed.target).data('textbox') ? $(ed.target).textbox('textbox') : $(ed.target)).focus();
+    //             }
+    //             editIndex = index;
+    //         } else {
+    //             setTimeout(function() {
+    //                 $('#dg').datagrid('selectRow', editIndex);
+    //             }, 0);
+    //         }
+    //     }
+    // }
 
-    function onEndEdit(index, row) {
-        var ed = $(this).datagrid('getEditor', {
-            index: index,
-            field: 'num_pedido'
-        });
-        row.nom_cliente = $(ed.target).combobox('getText');
-    }
+    // function onEndEdit(index, row) {
+    //     var ed = $(this).datagrid('getEditor', {
+    //         index: index,
+    //         field: 'num_pedido'
+    //     });
+    //     row.nom_cliente = $(ed.target).combobox('getText');
+    // }
 
     function append() {
-        if (endEditing()) {
-            $('#dg').datagrid('appendRow', {
-                status: 'P'
-            });
-            editIndex = $('#dg').datagrid('getRows').length - 1;
-            $('#dg').datagrid('selectRow', editIndex)
-                .datagrid('beginEdit', editIndex);
-        }
+        $('#win').window('refresh', 'controlar_pedido.php');
+        $('#win').window('open');
+        // $('#dg').datagrid('appendRow', {
+        //     status: 'P'
+        // });
+        // editIndex = $('#dg').datagrid('getRows').length - 1;
+        // $('#dg').datagrid('selectRow', editIndex)
+        //     .datagrid('beginEdit', editIndex);
     }
 
     function removeit() {
@@ -234,10 +229,13 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
         editIndex = undefined;
     }
 
-    function acceptit() {
-        if (endEditing()) {
-            $('#dg').datagrid('acceptChanges');
+    function edit() {
+        let num_pedido = $(this).data("num_pedido");
+        if(num_pedido == undefined){
+            return 
         }
+        $('#win').window('refresh', 'controlar_pedido.php?num_pedido=' + num_pedido);
+        $('#win').window('open');
     }
 
     function reject() {
@@ -245,10 +243,7 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
         editIndex = undefined;
     }
 
-    function getChanges() {
-        var rows = $('#dg').datagrid('getChanges');
-        alert(rows.length + ' rows are changed!');
-    }
+   
 
     $('#win').window({
         width: 600,
